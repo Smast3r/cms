1e1153ffc6ffdd25493b9a2a2321db2f0171c775
Saad Alshrideh <s14192009@gmail.com>
2021-05-31 19:48:27 +0300

    database migrations are done git add .

--- CMS/database/migrations/2021_05_31_164332_create_link_table.php
+++ CMS/database/migrations/2021_05_31_164332_create_link_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFilesTable extends Migration
+class CreateLinkTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateFilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('files', function (Blueprint $table) {
+        Schema::create('link', function (Blueprint $table) {
+
             $table->increments('id');
-            $table->string('name');
-            $table->string('category');
+            $table->integer('userId');
+            $table->integer('postId');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateFilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('files');
+        Schema::dropIfExists('link');
     }
 }
