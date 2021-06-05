f4925f72823b9ffe9b6c9581753750e5348a26cf
Saad Alshrideh <s14192009@gmail.com>
2021-06-05 06:34:11 +0300

    5th day of learning laravel

--- CMS/app/Models/User.php
+++ CMS/app/Models/User.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
+//class User extends Model
 {
     use HasFactory, Notifiable;
 
@@ -40,4 +42,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
 }
--- CMS/routes/web.php
+++ CMS/routes/web.php
@@ -17,12 +17,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/find',function() {
 
-    $Table =  User::where('id',1)->orderBy('id')->get();
+/*  this is how to retrieve data  ======================================*/
+Route::get('/findAll',function() {
+
+    //$Table =  User::where('id',1)->orderBy('id', 'desc')->get();
+      $Table =  User::orderBy('id', 'asc')->get(); //  this will bring all the things .
+    //$Table = User::where('id', '<' , 50)->firstOrFail() ;
     return $Table ;
 }) ;
 
+Route::get('find2' , function(){
+   $posts = User::findOrFail(1);
+   return $posts ;
+});
+
+/* retrieve ends =======================================================*/
+
+
+/* data insertion with Elequent ========================================*/
 Route::get('insert', function() {
 
     $date = date("D") ;
@@ -31,6 +44,77 @@ Route::get('insert', function() {
 
 });
 
+Route::get('/ins2',function (){
+
+    $Post = new User ;
+    $Post->name = 'SaadAgain';
+    $Post->email = 's14192009+1@gmail.com' ;
+    $Post->Password = 'S20102010';
+
+    $Post->save();
+
+
+
+});
+
+
+/* data insertion with Elequent ends ====================================*/
+
+
+/* data update with Elequent ================================***========*/
+Route::get('/update',function (){
+
+    $Post = User::find(1) ;
+    $Post->name = 'SaadUpdated';
+    $Post->email = 's14192009Updated@gmail.com' ;
+    $Post->Password = 'S20102010updated';
+
+    $Post->save();
+
+
+
+});
+
+Route::get('/update2' , function (){
+
+    // no need to fill all the parameters in case of updating
+    User::where('id',6)->update(['name'=>'saadFromUpdate2','email'=>'FU2@FU2.com']);
+
+});
+
+
+/* data update with Elequent ends ====================================*/
+
+/* Mass assignment operation or something */
+
+
+Route::get('/create',function (){
+
+User::create(['name'=>'saadCreatTest3', 'email'=>'create3@create.com' , 'password'=>'Whatever3']);
+
+});
+
+/* Mass assignment operation or something ends */
+
+
+/*delete data starts here */
+
+Route::get('/delete/{id}',function($id){
+
+    $User = User::find($id) ;
+    $User->delete();
+});
+
+
+//other way
+
+Route::get('delete2/{id}',function ($id){
+
+    User::destroy($id) ; 
+});
+
+
+/* delete data ends here */
 
 Route::get('/dashboard', function () {
     return view('dashboard');
